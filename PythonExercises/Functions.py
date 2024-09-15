fbf408a178a745e2d927d77e05517e2b03b62a8b
surya2 <surya.selvam03@gmail.com>
2024-09-14 23:28:33 -0400

    New Python tasks for first day

--- PythonExercises/Functions.py
+++ PythonExercises/Functions.py
@@ -13,4 +13,24 @@ def getSkibonacci(n):
         return 1
     else:
         return 2*getSkibonacci(n-2) + getSkibonacci(n-3)
-    
+
+#Task 5
+def parseMessageArray(msgArr):
+    #Your code goes here
+    pass
+
+#Task 5
+def countAllPossiblePaths(grid):
+    #Your code goes here
+    pass
+#Task 6
+def consolidateDuplicateEntries():
+    #Your code goes here
+    pass
+
+#Task 7
+def replaceAllReferences():
+    #Your code goes here
+    pass
+
+
